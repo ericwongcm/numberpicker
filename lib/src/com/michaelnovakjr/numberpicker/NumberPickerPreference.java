ff00176aff061b999df7935ed9d634031b5fc412
ericwongcm <ericwongcm@gmail.com>
2014-03-21 23:58:51 +1100

    Add hour/minute/second
    Added calculations to convert hour/minute to second and sum all values together.
    Limit to 60 min and 60 seconds for min/sec picker.
    Disabled max range exception

--- lib/src/com/michaelnovakjr/numberpicker/NumberPickerPreference.java
+++ lib/src/com/michaelnovakjr/numberpicker/NumberPickerPreference.java
@@ -5,11 +5,12 @@ import android.content.res.TypedArray;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.TextView;
 
 public class NumberPickerPreference extends DialogPreference {
-    private NumberPicker mPicker;
+    private NumberPicker hPicker, mPicker, sPicker;
     private int mStartRange;
-    private int mEndRange;
+//    private int mEndRange;
     private int mDefault;
 
     public NumberPickerPreference(Context context, AttributeSet attrs, int defStyle) {
@@ -21,9 +22,9 @@ public class NumberPickerPreference extends DialogPreference {
 
         TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.numberpicker);
         mStartRange = arr.getInteger(R.styleable.numberpicker_startRange, 0);
-        mEndRange = arr.getInteger(R.styleable.numberpicker_endRange, 200);
+//        mEndRange = arr.getInteger(R.styleable.numberpicker_endRange, 900000);
         mDefault = arr.getInteger(R.styleable.numberpicker_defaultValue, 0);
-
+        
         arr.recycle();
 
         setDialogLayoutResource(R.layout.pref_number_picker);
@@ -40,9 +41,22 @@ public class NumberPickerPreference extends DialogPreference {
     @Override
     protected void onBindDialogView(View view) {
         super.onBindDialogView(view);
-        mPicker = (NumberPicker) view.findViewById(R.id.pref_num_picker);
-        mPicker.setRange(mStartRange, mEndRange);
-        mPicker.setCurrent(getValue());
+        hPicker = (NumberPicker) view.findViewById(R.id.pref_num_picker_hour);
+        hPicker.setRange(mStartRange, 240);
+        hPicker.setCurrent(getValue()/3600);
+
+        mPicker = (NumberPicker) view.findViewById(R.id.pref_num_picker_minute);
+        mPicker.setRange(mStartRange, 60);
+        mPicker.setCurrent(getValue()%3600/60);
+        
+        sPicker = (NumberPicker) view.findViewById(R.id.pref_num_picker_second);
+        sPicker.setRange(mStartRange, 60);
+        sPicker.setCurrent(getValue()%60);
+        
+//Shows the saved value on dialog
+        TextView total_second = (TextView) view.findViewById(R.id.pref_total_second);
+        total_second.setText(String.valueOf(getValue()) + " seconds");
+        
     }
 
     @Override
@@ -50,24 +64,31 @@ public class NumberPickerPreference extends DialogPreference {
         super.onDialogClosed(positiveResult);
 
         final int origValue = getValue();
-        final int curValue = mPicker.getCurrent();
-
+        final int curValue = sPicker.getCurrent() + mPicker.getCurrent()*60 + hPicker.getCurrent()*3600;
+        
         if (positiveResult && (curValue != origValue)) {
             if (callChangeListener(curValue)) {
                 saveValue(curValue);
+//                System.out.println("DEBUG curValue" + String.valueOf(curValue));// DEBUG
             }
+            
         }
     }
 
-    public void setRange(int start, int end) {
-        mPicker.setRange(start, end);
-    }
+/*    public void setRange(int start, int end) {
+    	hPicker.setRange(start, end);
+    	mPicker.setRange(start, end);
+        sPicker.setRange(start, end);
+//        mPicker.setRange(start, end);
+    }*/
 
     private boolean saveValue(int val) {
-        return persistInt(val);
+//    	System.out.println("DEBUG val" + String.valueOf(val));// DEBUG
+    	return persistInt(val);
     }
-
+    
     private int getValue() {
         return getSharedPreferences().getInt(getKey(), mDefault);
     }
+
 }
